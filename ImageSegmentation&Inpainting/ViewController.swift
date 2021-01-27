39b4f6a6af1c5878697be05e9f49e5940073a2c6
zero <zero@smail.nju.edu.cn>
2021-01-28 00:30:17 +0800

    Initial Commit

---
+++ ImageSegmentation&Inpainting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageSegmentation&Inpainting
+//
+//  Created by zero on 2021/1/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
